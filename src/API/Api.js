858a4dbce918209a6493c7258f32d5c5e8a041c5
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2023-01-09 11:17:40 +0530

    Delete src directory

--- src/API/Api.js
+++
@@ -1,4 +0,0 @@
-
-export const userData =()=>`https://randomuser.me/api/?page=1&results=50`
-export const get_Capsul_Data =(page, status)=> `https://api.spacexdata.com/v3/capsules?page=${page}&limit=20`
-export const singal_caps_DAta =(capsule_serial)=>  `https://api.spacexdata.com/v3/capsules/${capsule_serial}`
